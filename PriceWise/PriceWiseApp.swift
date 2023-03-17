a695bb0b3e8f87c05fbb82c346cf7e2b0d481370
Asa Barton <arbarton@uci.edu>
2023-03-16 22:58:34 -0700

    Initial Commit

---
+++ PriceWise/PriceWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceWiseApp.swift
+//  PriceWise
+//
+//  Created by Asa Barton on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PriceWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

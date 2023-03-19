9d2a71420f26a03fb7ee10fcee507fa643c6d73b
Asa Barton <arbarton@uci.edu>
2023-03-18 20:48:55 -0700

    Initial Commit

--- PriceWise/ContentView.swift
+++ PriceWise/ContentView.swift
@@ -1,19 +1,87 @@
-//
-//  ContentView.swift
-//  PriceWise
-//
-//  Created by Asa Barton on 3/16/23.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View
+{
+    @State private var item1Price = ""
+    @State private var item1Weight = ""
+    @State private var item2Price = ""
+    @State private var item2Weight = ""
+    @State private var message = ""
+    
+    struct Item: Equatable
+    {
+        var price: Double
+        var weight: Double
+    }
+    
+    func findBetterDeal(item1: Item, item2: Item) -> Item? {
+        let item1PricePerWeight = item1.price / item1.weight
+        let item2PricePerWeight = item2.price / item2.weight
+        if item1PricePerWeight == item2PricePerWeight {
+            return nil
+        } else {
+            return item1PricePerWeight < item2PricePerWeight ? item1 : item2
+        }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Item 1")
+                    TextField("Price", text: $item1Price)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                    TextField("Weight", text: $item1Weight)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("Item 2")
+                    TextField("Price", text: $item2Price)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                    TextField("Weight", text: $item2Weight)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                }
+            }
+            Button(action: {
+                guard let item1Price = Double(item1Price),
+                      let item1Weight = Double(item1Weight),
+                      let item2Price = Double(item2Price),
+                      let item2Weight = Double(item2Weight)
+                else {
+                    message = "Please enter valid numbers for both items"
+                    return
+                }
+                
+                let item1 = Item(price: item1Price, weight: item1Weight)
+                let item2 = Item(price: item2Price, weight: item2Weight)
+                
+                guard let betterDeal = findBetterDeal(item1: item1, item2: item2)
+                else {
+                    message = "The two items have the same value per weight"
+                    return
+                }
+                
+                message = "\(betterDeal == item1 ? "Item 1" : "Item 2") is a better deal"
+            }) {
+                Text("Compare Items")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 18))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 2)
+                    )
+            }
+            .padding()
+            Text(message)
         }
         .padding()
     }
@@ -24,3 +92,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- PriceWise/PriceWiseApp.swift
+++ PriceWise/PriceWiseApp.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 @main
-struct PriceWiseApp: App {
-    var body: some Scene {
-        WindowGroup {
+struct PriceWiseApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
             ContentView()
         }
     }
